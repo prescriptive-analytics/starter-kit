d1f998174c5d05a65194d804beeebf20f946e68e
Guanjie Zheng <gjzheng93@gmail.com>
2020-05-02 10:12:30 +0800

    clean some unnecessary files

--- submit.py
+++ submit.py
@@ -4,20 +4,24 @@ import json
 
 period = 840
 
-engine = simulator.Engine()
+for round in range(10):
+    if round == 0:
+        engine = simulator.Engine()
+    else:
+        engine = simulator.Engine(write_mode="append")
 
-engine.reset()
-for i in range(period):
-    engine.next_step()
-    print(engine.get_current_time())
-    print(engine.get_man_visited_history(1))
-    print(engine.get_man_infection_state(1))
-    print(engine.get_man_visited_history(1))
-    print(engine.get_region_infected_cnt(1))
+    engine.reset()
+    for i in range(period):
+        engine.next_step()
+        print(engine.get_current_time())
+        print(engine.get_man_visited_history(1))
+        print(engine.get_man_infection_state(1))
+        print(engine.get_man_visited_history(1))
+        print(engine.get_region_infected_cnt(1))
 
-    engine.set_man_confine_days({1: 5}) # {manID: day}
-    engine.set_man_quarantine_days({2: 5}) # {manID: day}
-    engine.set_man_isolate_days({3: 5}) # {manID: day}
-    engine.set_man_to_treat({4: True}) # {manID: day}
+        engine.set_man_confine_days({1: 5}) # {manID: day}
+        engine.set_man_quarantine_days({2: 5}) # {manID: day}
+        engine.set_man_isolate_days({3: 5}) # {manID: day}
+        engine.set_man_to_treat({4: True}) # {manID: day}
 
-del engine
+    del engine
