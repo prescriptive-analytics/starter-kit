3e4d2bf3ea56f60fccaa937885b89ca91ed31110
Guanjie Zheng <gjzheng93@gmail.com>
2020-05-03 20:09:44 +0800

    add back submit.py

--- example.py
+++ example.py
@@ -4,16 +4,16 @@ import json
 
 period = 840
 
-engine = simulator.Engine()
+engine = simulator.Engine(thread_num=1, write_mode="write", specified_run_name="test")
 
 engine.reset()
 for i in range(period):
     engine.next_step()
-    print(engine.get_current_time())
-    print(engine.get_man_visited_history(1))
-    print(engine.get_man_infection_state(1))
-    print(engine.get_man_visited_history(1))
-    print(engine.get_region_infected_cnt(1))
+    engine.get_current_time()
+    engine.get_man_visited_history(1)
+    engine.get_man_infection_state(1)
+    engine.get_man_visited_history(1)
+    engine.get_region_infected_cnt(1)
 
     engine.set_man_confine_days({1: 5}) # {manID: day}
     engine.set_man_quarantine_days({2: 5}) # {manID: day}
---
+++ submit.py
@@ -0,0 +1,27 @@
+import simulator
+import os
+import json
+
+period = 840
+
+for round in range(10):
+    if round == 0:
+        engine = simulator.Engine(thread_num=1, write_mode="write", specified_run_name="test")
+    else:
+        engine = simulator.Engine(thread_num=1, write_mode="append", specified_run_name="test")
+
+    engine.reset()
+    for i in range(period):
+        engine.next_step()
+        engine.get_current_time()
+        engine.get_man_visited_history(1)
+        engine.get_man_infection_state(1)
+        engine.get_man_visited_history(1)
+        engine.get_region_infected_cnt(1)
+
+        engine.set_man_confine_days({1: 5}) # {manID: day}
+        engine.set_man_quarantine_days({2: 5}) # {manID: day}
+        engine.set_man_isolate_days({3: 5}) # {manID: day}
+        engine.set_man_to_treat({4: True}) # {manID: day}
+
+    del engine
