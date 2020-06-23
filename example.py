bfeffc98f3cdf266ecadf9e7ee53d48c4e9c8e19
Guanjie Zheng <gjzheng93@gmail.com>
2020-06-23 11:40:42 +0800

    update example.py with some comments

--- example.py
+++ example.py
@@ -10,11 +10,18 @@ engine.reset()
 for i in range(period):
     engine.next_step()
     engine.get_current_time()
-    engine.get_individual_visited_history(1)
-    engine.get_individual_infection_state(1)
-    engine.get_individual_visited_history(1)
-    engine.get_area_infected_cnt(1)
 
+    # Here, we give the example to get the information about individual with id 1
+    individual_id = 1
+    engine.get_individual_visited_history(individual_id)
+    engine.get_individual_infection_state(individual_id)
+    engine.get_individual_visited_history(individual_id)
+
+    # Here, we give the example to get the information about region with id 1
+    region_id = 1
+    engine.get_area_infected_cnt(region_id)
+
+    # Here, we give the example to set actions for individual with id 1, 2, 3, and 4 respectively
     engine.set_individual_confine_days({1: 5}) # {individualID: day}
     engine.set_individual_quarantine_days({2: 5}) # {individualID: day}
     engine.set_individual_isolate_days({3: 5}) # {individualID: day}
